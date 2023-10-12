eca9f829febc738b0d45c353bf715f7efa3aef87
unknown <you@example.com>
2023-10-12 18:13:13 +0200

    last version

--- index.js
+++ index.js
@@ -7,11 +7,25 @@ function addTask () {
     if (task !=="") {
         const li = document.createElement("li")
         li.innerHTML = `
+        <input type="checkbox" class="markBtn" />
         <span class=task>${task}</span>
         <button class="removeBtn" onclick="removeTask(this)">X</button>
-       ` ;
+        <button class="editBtn" onclick="editTask(this)">E</button>` ;
         taskList.appendChild(li)
         taskInput.value = "";
+        // const markBtn = li.querySelector(".markBtn");
+        // markBtn.addEventListener("change", function() {
+        //     if (this.checked) {
+        //         li.classList.add("completed");
+        //     } else {
+        //         li.classList.remove("completed");
+        //     }
+        // });
+        const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+
+
     }
 }
 
@@ -19,3 +33,31 @@ function removeTask(btn) {
     btn.parentNode.remove();
 }
 
+function editTask(btn) {
+    const task = btn.parentNode;
+    const taskText = task.querySelector(".task");
+    const editInput = document.createElement("input");
+    editInput.value = taskText.textContent;
+    task.replaceChild(editInput, taskText);
+    editInput.focus();
+    editInput.addEventListener("blur", function() {
+        taskText.textContent = editInput.value;
+        task.replaceChild(taskText, editInput);
+    });
+}
+
+const loadBtn = document.getElementById("loadBtn");
+
+loadBtn.addEventListener("click", function() {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.forEach(task => {
+        // Add the task to the task list
+        const li = document.createElement("li");
+        li.innerHTML = `
+        <input type="checkbox" class="markBtn" />
+        <span class="task">${task}</span>
+        <button class="removeBtn" onclick="removeTask(this)">X</button>
+        <button class="editBtn" onclick="editTask(this)">E</button>`;
+        taskList.appendChild(li);
+    });
+});
